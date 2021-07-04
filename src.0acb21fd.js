08341dbb6dd13625aa7295d4413a1f15cfe3c428
AnnaYegorova <82411573+AnnaYegorova@users.noreply.github.com>
2021-07-04 20:23:59 +0000

    Deploying to gh-pages from @ AnnaYegorova/team-project-group-1@858007b2a0548f13495f4acd05cc0bea04cf23de 🚀

--- src.0acb21fd.js
+++ src.0acb21fd.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-group-1/src.094cda08.js.map
+//# sourceMappingURL=/team-project-group-1/src.0acb21fd.js.map
